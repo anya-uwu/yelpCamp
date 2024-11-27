e2103b60c61127cee886b3fdb26f5547c9b1255d
anya-uwu <anyadev@proton.me>
2024-11-27 18:22:47 -0500

    uhhhh

--- models/campground.js
+++ models/campground.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CampGroundSchema = new Schema({
+const CampgroundSchema = new Schema({
     title: String,
     price: String,
     description: String,
