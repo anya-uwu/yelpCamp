84c91b9442a1d61d29a8fcd8ab88f9ef7f9f8601
anya-uwu <anyadev@proton.me>
2024-11-27 22:33:41 -0500

    created basic routes to show campsites and details

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -20,11 +21,15 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'my backyard', description: 'cheap camping'});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
 });
+
+app.get('/campgrounds/:id', async (req, res,) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
+})
 
 app.listen(3000, () => {
     console.log('serving port 3000')
