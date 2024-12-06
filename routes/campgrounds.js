1f6d0f7c5c52ba31fff5accef685a07b603561ee
anya-uwu <anyadev@proton.me>
2024-12-06 18:02:59 -0500

    working on image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const campgrounds = require('../controllers/campgrounds')
+const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utilities/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('it worked!')
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
