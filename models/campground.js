d3b205ca45241c32b231ff21446db6b3046a7f75
anya-uwu <anyadev@proton.me>
2024-12-09 20:53:09 -0500

    added delete image feature

--- models/campground.js
+++ models/campground.js
@@ -3,19 +3,23 @@ const Review = require('./review');
 const { ref } = require('joi');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
     author: {
-        type:Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: 'User'
     },
     reviews: [
@@ -26,8 +30,8 @@ const CampgroundSchema = new Schema({
     ]
 });
 
-CampgroundSchema.post('findOneAndDelete', async function(doc) {
-    if(doc) {
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
         await Review.deleteMany({
             _id: {
                 $in: doc.reviews
